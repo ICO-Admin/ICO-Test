0fedf4a2a0db3b595b51c24ab164bd62fb54f33e
ICO-admin <nicolas@ico.be>
2020-04-23 10:52:31 +0200

    test branch 2 modif

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-   res.send('Accueil1');
+   res.send('Accueil test-branch2');
 });
 
 module.exports = router;
